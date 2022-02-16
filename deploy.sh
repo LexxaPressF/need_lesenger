3cb1413d4759e3bcc898743bb146effbf14e5b02
@_@/ <57321062+LexxaPressF@users.noreply.github.com>
2022-02-16 20:01:36 +0300

    Deploy 2 try

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
- git push -f git@github.com:<USERNAME>/need-nesenger.git master
+# git push -f git@github.com:<USERNAME>/need-nesenger.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+ git push -f git@github.com:LexxaPressF/need_lesenger.git master:gh-pages
 
 cd -
